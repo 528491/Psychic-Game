d0744e1addf60050624b067df14afa0726684dcd
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-03-11 14:50:29 -0700

    Game play functionality is complete, just need to polish off a few minor bugs.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 // Javascript for Psychic Game
 
-
 //Initialize Variables we will use
 var wins = 0; //Initially, user has not made any guesses, and hence no wins
 var losses = 0;
@@ -18,15 +17,77 @@ function returnRandomLowercaseLetter(){
     return possibleLetters[Math.floor(Math.random()*possibleLetters.length)];
 }
 
-//Player Wins
+//This function is only run once, just before the eventlistener begins the loop that will encompass the rest of the gameplay
+//Using a function instead of direct initialization as I wanted all the variables to be initialized at once and that's not possible with psychic guess
+function initializeGameplay(){
+    wins = 0;
+    losses = 0;
+    guessesLeft = 9;
+    yourGuessesSoFar = [];
+    psychicGuess = returnRandomLowercaseLetter();
+}
+
+function correctGuess(){
+    wins++;
+    guessesLeft = 9;
+    yourGuessesSoFar = [];
+}
+
+function incorrectGuess(){
+    guessesLeft--;
+
+}
+
+//Update the variables on the page to match what they are recorded as in this script
+function refreshDisplay(){
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById("guessesSoFar").innerHTML = "Guesses so Far: " + yourGuessesSoFar;
+}
+
+//New Game Function
+//Win or Loss, indicated by 'w' or 'l'
 //Clear yourGuessesSoFar
 //Increase wins counter
 //Refresh Guesses so far array (empty it)
+//Choose new computer guess
+function newGame(winOrLoss){
+    //winOrLoss refers to the win condition of the human, not the computer player
 
-//Player Loses
-//Clear yourGuessesSoFar
-//Increase losses counter
-//Refresh Guesses so far array (empty it)
+    //if win
+    if (winOrLoss == 'w'){
+        yourGuessesSoFar = [];
+        psychicGuess = returnRandomLowercaseLetter();
+        refreshDisplay();
+    }
+    //if loss
+    if (winOrLoss == 'l'){
+        losses++;
+        yourGuessesSoFar = [];
+        guessesLeft = 9;
+        psychicGuess = returnRandomLowercaseLetter();
+        refreshDisplay();
+    }
+
+}
 
 //Event listener for gameplay functionlity
 //Inside event that is triggered by keystrokes, check if player has won or lost each time
+initializeGameplay();
+document.onkeyup = function(event){
+    var userGuess = event.key;
+    yourGuessesSoFar.push(userGuess);
+    refreshDisplay();
+
+    if (userGuess == psychicGuess) {
+        correctGuess();
+        newGame('w');
+    }
+    else {
+        incorrectGuess();
+        if (guessesLeft == 0){
+            newGame('l');
+        }
+    }
+}
